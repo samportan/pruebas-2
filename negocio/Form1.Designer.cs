0b536b96b20802194151961ae83e2e7e929e4c43
samportan <50893014+samportan@users.noreply.github.com>
2020-05-25 21:22:10 -0600

    update

--- negocio/Form1.Designer.cs
+++ negocio/Form1.Designer.cs
@@ -38,59 +38,63 @@
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(293, 126);
+            this.label1.Location = new System.Drawing.Point(330, 126);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 26);
+            this.label1.Size = new System.Drawing.Size(102, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario: ";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(293, 181);
+            this.label2.Location = new System.Drawing.Point(330, 181);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 26);
+            this.label2.Size = new System.Drawing.Size(102, 26);
             this.label2.TabIndex = 1;
             this.label2.Text = "Contraseña:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // UserTextBox
             // 
-            this.UserTextBox.Location = new System.Drawing.Point(395, 130);
+            this.UserTextBox.Location = new System.Drawing.Point(444, 130);
             this.UserTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.UserTextBox.Name = "UserTextBox";
-            this.UserTextBox.Size = new System.Drawing.Size(167, 22);
+            this.UserTextBox.Size = new System.Drawing.Size(187, 22);
             this.UserTextBox.TabIndex = 2;
             // 
             // PasswordTxt
             // 
-            this.PasswordTxt.Location = new System.Drawing.Point(395, 185);
+            this.PasswordTxt.Location = new System.Drawing.Point(444, 185);
             this.PasswordTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.PasswordTxt.Name = "PasswordTxt";
-            this.PasswordTxt.Size = new System.Drawing.Size(167, 22);
+            this.PasswordTxt.Size = new System.Drawing.Size(187, 22);
             this.PasswordTxt.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(463, 242);
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Location = new System.Drawing.Point(521, 242);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 28);
+            this.button1.Size = new System.Drawing.Size(111, 28);
             this.button1.TabIndex = 4;
             this.button1.Text = "Iniciar sesion";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(667, 405);
+            this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (64)))), ((int) (((byte) (64)))), ((int) (((byte) (64)))));
+            this.ClientSize = new System.Drawing.Size(750, 405);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.PasswordTxt);
             this.Controls.Add(this.UserTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.ForeColor = System.Drawing.Color.White;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
